18f07f4fd7fe823e73f6ec0104f2d5dc52d0e978
Sri Girish Tangirala <srigirish2797@gmail.com>
2021-11-22 17:58:52 +0100

    first commit

---
+++ ProcessA/bin/arduino-mega2560/arduino/base.d
@@ -0,0 +1,195 @@
+/home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/arduino/base.o: \
+ /home/girish/RIOT-spi/sys/arduino/base.cpp \
+ /home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/riotbuild/riotbuild.h \
+ /home/girish/RIOT-spi/sys/include/xtimer.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdbool.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /home/girish/RIOT-spi/sys/include/timex.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/inttypes.h \
+ /usr/lib/avr/include/inttypes.h /home/girish/RIOT-spi/core/include/msg.h \
+ /home/girish/RIOT-spi/core/include/kernel_types.h \
+ /usr/lib/gcc/avr/5.4.0/include-fixed/limits.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/sys/types.h \
+ /home/girish/RIOT-spi/core/include/mutex.h \
+ /home/girish/RIOT-spi/core/include/list.h \
+ /home/girish/RIOT-spi/core/include/kernel_types.h \
+ /home/girish/RIOT-spi/boards/arduino-mega2560/include/board.h \
+ /home/girish/RIOT-spi/boards/common/arduino-atmega/include/board_common.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/cpu.h \
+ /usr/lib/avr/include/stdio.h /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/avr/include/avr/interrupt.h /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom2560.h \
+ /usr/lib/avr/include/avr/iomxx0_1.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
+ /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/cpu_conf.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/atmega_regs_common.h \
+ /usr/lib/avr/include/avr/power.h \
+ /home/girish/RIOT-spi/core/include/sched.h \
+ /home/girish/RIOT-spi/core/include/kernel_defines.h \
+ /home/girish/RIOT-spi/core/include/bitarithm.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/cpu_conf.h \
+ /home/girish/RIOT-spi/core/include/native_sched.h \
+ /home/girish/RIOT-spi/core/include/clist.h \
+ /home/girish/RIOT-spi/core/include/thread.h \
+ /home/girish/RIOT-spi/core/include/cib.h \
+ /home/girish/RIOT-spi/core/include/assert.h \
+ /home/girish/RIOT-spi/core/include/panic.h \
+ /home/girish/RIOT-spi/core/include/msg.h \
+ /home/girish/RIOT-spi/core/include/sched.h \
+ /home/girish/RIOT-spi/core/include/irq.h \
+ /home/girish/RIOT-spi/boards/common/arduino-atmega/include/arduino_pinmap.h \
+ /home/girish/RIOT-spi/drivers/include/periph/gpio.h \
+ /home/girish/RIOT-spi/cpu/atmega2560/include/periph_cpu.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/periph_cpu_common.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/include/cpu.h \
+ /home/girish/RIOT-spi/boards/common/arduino-atmega/include/periph_conf.h \
+ /home/girish/RIOT-spi/boards/common/atmega/include/periph_conf_atmega_common.h \
+ /home/girish/RIOT-spi/drivers/include/periph/adc.h \
+ /home/girish/RIOT-spi/sys/include/xtimer/tick_conversion.h \
+ /home/girish/RIOT-spi/sys/include/div.h \
+ /home/girish/RIOT-spi/core/include/assert.h \
+ /home/girish/RIOT-spi/sys/include/xtimer/implementation.h \
+ /home/girish/RIOT-spi/drivers/include/periph/timer.h \
+ /home/girish/RIOT-spi/drivers/include/periph/dev_enums.h \
+ /home/girish/RIOT-spi/drivers/include/periph/pwm.h \
+ /home/girish/RIOT-spi/sys/arduino/include/arduino.hpp \
+ /home/girish/RIOT-spi/boards/common/arduino-atmega/include/arduino_board.h \
+ /home/girish/RIOT-spi/sys/arduino/include/serialport.hpp \
+ /home/girish/RIOT-spi/core/include/ringbuffer.h \
+ /home/girish/RIOT-spi/drivers/include/periph/uart.h
+
+/home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/riotbuild/riotbuild.h:
+
+/home/girish/RIOT-spi/sys/include/xtimer.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdbool.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/home/girish/RIOT-spi/sys/include/timex.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/inttypes.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/home/girish/RIOT-spi/core/include/msg.h:
+
+/home/girish/RIOT-spi/core/include/kernel_types.h:
+
+/usr/lib/gcc/avr/5.4.0/include-fixed/limits.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/sys/types.h:
+
+/home/girish/RIOT-spi/core/include/mutex.h:
+
+/home/girish/RIOT-spi/core/include/list.h:
+
+/home/girish/RIOT-spi/core/include/kernel_types.h:
+
+/home/girish/RIOT-spi/boards/arduino-mega2560/include/board.h:
+
+/home/girish/RIOT-spi/boards/common/arduino-atmega/include/board_common.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/cpu.h:
+
+/usr/lib/avr/include/stdio.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/avr/iom2560.h:
+
+/usr/lib/avr/include/avr/iomxx0_1.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/cpu_conf.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/atmega_regs_common.h:
+
+/usr/lib/avr/include/avr/power.h:
+
+/home/girish/RIOT-spi/core/include/sched.h:
+
+/home/girish/RIOT-spi/core/include/kernel_defines.h:
+
+/home/girish/RIOT-spi/core/include/bitarithm.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/cpu_conf.h:
+
+/home/girish/RIOT-spi/core/include/native_sched.h:
+
+/home/girish/RIOT-spi/core/include/clist.h:
+
+/home/girish/RIOT-spi/core/include/thread.h:
+
+/home/girish/RIOT-spi/core/include/cib.h:
+
+/home/girish/RIOT-spi/core/include/assert.h:
+
+/home/girish/RIOT-spi/core/include/panic.h:
+
+/home/girish/RIOT-spi/core/include/msg.h:
+
+/home/girish/RIOT-spi/core/include/sched.h:
+
+/home/girish/RIOT-spi/core/include/irq.h:
+
+/home/girish/RIOT-spi/boards/common/arduino-atmega/include/arduino_pinmap.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/gpio.h:
+
+/home/girish/RIOT-spi/cpu/atmega2560/include/periph_cpu.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/periph_cpu_common.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/include/cpu.h:
+
+/home/girish/RIOT-spi/boards/common/arduino-atmega/include/periph_conf.h:
+
+/home/girish/RIOT-spi/boards/common/atmega/include/periph_conf_atmega_common.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/adc.h:
+
+/home/girish/RIOT-spi/sys/include/xtimer/tick_conversion.h:
+
+/home/girish/RIOT-spi/sys/include/div.h:
+
+/home/girish/RIOT-spi/core/include/assert.h:
+
+/home/girish/RIOT-spi/sys/include/xtimer/implementation.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/timer.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/dev_enums.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/pwm.h:
+
+/home/girish/RIOT-spi/sys/arduino/include/arduino.hpp:
+
+/home/girish/RIOT-spi/boards/common/arduino-atmega/include/arduino_board.h:
+
+/home/girish/RIOT-spi/sys/arduino/include/serialport.hpp:
+
+/home/girish/RIOT-spi/core/include/ringbuffer.h:
+
+/home/girish/RIOT-spi/drivers/include/periph/uart.h:
---
+++ ProcessA/bin/arduino-mega2560/core/assert.d
@@ -0,0 +1,33 @@
+/home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/core/assert.o: \
+ /home/girish/RIOT-spi/core/assert.c \
+ /home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/riotbuild/riotbuild.h \
+ /usr/lib/avr/include/stdio.h \
+ /home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/inttypes.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /usr/lib/avr/include/stdint.h /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /home/girish/RIOT-spi/core/include/assert.h \
+ /home/girish/RIOT-spi/core/include/panic.h \
+ /home/girish/RIOT-spi/core/include/kernel_defines.h
+
+/home/girish/RIOT_trial/ProcessA/bin/arduino-mega2560/riotbuild/riotbuild.h:
+
+/usr/lib/avr/include/stdio.h:
+
+/home/girish/RIOT-spi/cpu/atmega_common/avr_libc_extra/include/inttypes.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/home/girish/RIOT-spi/core/include/assert.h:
+
+/home/girish/RIOT-spi/core/include/panic.h:
+
+/home/girish/RIOT-spi/core/include/kernel_defines.h:
