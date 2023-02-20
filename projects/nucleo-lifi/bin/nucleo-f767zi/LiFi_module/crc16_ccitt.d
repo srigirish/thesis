cfee474b34d0f068c160a47774dea845dc6b9f13
tangiral1 <sri.tangirala@ovgu.de>
2023-02-20 18:27:31 +0100

    Adding projects folder

---
+++ projects/nucleo-lifi/bin/nucleo-f767zi/LiFi_module/crc16_ccitt.d
@@ -0,0 +1,14 @@
+/home/girish/RIOT/projects/nucleo-lifi/bin/nucleo-f767zi/LiFi_module/crc16_ccitt.o: \
+ /home/girish/RIOT/drivers/LiFi_module/crc16_ccitt.cpp \
+ /home/girish/RIOT/projects/nucleo-lifi/bin/nucleo-f767zi/riotbuild/riotbuild.h \
+ /home/girish/RIOT/drivers/LiFi_module/include/crc16_ccitt.hpp \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h
+
+/home/girish/RIOT/projects/nucleo-lifi/bin/nucleo-f767zi/riotbuild/riotbuild.h:
+
+/home/girish/RIOT/drivers/LiFi_module/include/crc16_ccitt.hpp:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
---
+++ projects/ota_test/bin/nucleo-f767zi/c25519/c25519.d
@@ -0,0 +1,39 @@
+/home/girish/RIOT/projects/ota_test/bin/nucleo-f767zi/c25519/c25519.o: \
+ /home/girish/RIOT/build/pkg/c25519/src/c25519.c \
+ /home/girish/RIOT/build/pkg/c25519/src/c25519.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/girish/RIOT/build/pkg/c25519/src/f25519.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h \
+ /home/girish/RIOT/core/lib/include/assert.h \
+ /usr/include/newlib/sys/cdefs.h /usr/include/newlib/sys/_locale.h \
+ /usr/include/newlib/strings.h /usr/include/newlib/sys/string.h
+/home/girish/RIOT/build/pkg/c25519/src/c25519.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/home/girish/RIOT/build/pkg/c25519/src/f25519.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/girish/RIOT/core/lib/include/assert.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/sys/_locale.h:
+/usr/include/newlib/strings.h:
+/usr/include/newlib/sys/string.h:
