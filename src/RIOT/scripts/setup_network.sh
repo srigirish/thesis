5ddf7724a2bda824b811d13109efab8c159ffd00
srigirish <56837355+srigirish@users.noreply.github.com>
2024-04-24 09:54:57 +0200

    Update setup_network.sh

--- src/RIOT/scripts/setup_network.sh
+++ src/RIOT/scripts/setup_network.sh
@@ -2,4 +2,4 @@
 cd ..
 make -C dist/tools/ethos clean all
 make -C dist/tools/uhcpd clean all
-echo marshall! | sudo -S dist/tools/ethos/setup_network.sh riot0 2001:db8::/64
+echo <sudo_pw> | sudo -S dist/tools/ethos/setup_network.sh riot0 2001:db8::/64
