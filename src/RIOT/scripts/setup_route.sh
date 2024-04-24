0e49bff7be113d6cc7905ade8e947bef30a125a1
srigirish <56837355+srigirish@users.noreply.github.com>
2024-04-24 09:56:02 +0200

    Update setup_route.sh

--- src/RIOT/scripts/setup_route.sh
+++ src/RIOT/scripts/setup_route.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo marshall! | sudo -S ip route add 2001:db1::/64 via fe80::a6cf:12ff:fe9a:2dc8 dev wlp0s20f3
+echo <sudo_pw> | sudo -S ip route add 2001:db1::/64 via fe80::a6cf:12ff:fe9a:2dc8 dev wlp0s20f3
 sudo ip route add 2001:db2::/64 via fe80::266f:28ff:fe95:d118 dev wlp0s20f3
 sudo ip route add 2001:db3::/64 via fe80::963c:c6ff:fe34:d060 dev wlp0s20f3
 sudo ip route add 2001:db4::/64 via fe80::caf0:9eff:fe74:ef14 dev wlp0s20f3
